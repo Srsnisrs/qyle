497db7c8f96d3883262d37703d6cc87bf182f0cc
Srsnisrs <122684957+Srsnisrs@users.noreply.github.com>
2023-01-15 08:20:06 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -1,15 +1,51 @@
 let handler = async m => m.reply(`
-╭─「 RENT BOT 」
-│
-│ > Harga :
-│• 5K / Minggu
-│• 20K / Bulan
-│• 30K / Permanent
-│
-│ > Pembayaran :
-│• DANA : 08818770766
-│• TRI : 089520306297
+𝖖𝖞𝖑𝖊𝖓𝖘𝖊𝖒𝖇𝖔𝖙𝖟
+
+ *• SEWA BOT & UP TO PREMIUM •*
+
+        
+
+╭─「 SEWABOT • 」
+
+│ • Maxis [ 0177302593 ] 
+
+│ • Umobile  [PM OWNER]
+
+│ • Celcom  [PM OWNER]
+
+│ • Digi  [PM OWNER]
+
 ╰────
+
+╭─「 *BASIC PRICE* 」
+
+│ > 1 Minggu - RM 3:00 / 4 Group
+
+│ > 3 Minggu - RM 5:00 / 6 Group
+
+│ > Unlimited  - RM 11:00 / Unlimited Group
+
+│ 
+
+╰────
+
+╭─「 *PREMIUM PRICE* 」
+
+│ > 1 Minggu - RM 7:00 / 6 Group
+
+│ > 3 Minggu - RM 9:00 / 10 Group
+
+│ > Unlimited - RM 15:00 / Unlimited Group 
+
+│ 
+
+╰────
+
+wa.me/60177302593
+
+*Bukan Bot!!!*
+
+*Owner 𝖖𝖞𝖑𝖊𝖓𝖘𝖊𝖒𝖇𝖔𝖙𝖟*
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['sewa']
 handler.tags = ['info']
